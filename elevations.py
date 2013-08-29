2e20b1f4baa3f6add1475744a0c952b049208bb9
Peter France <pfrance@gmail.com>
2013-08-29 11:24:40 -0400

    clean up elevation.py

--- elevations.py
+++ elevations.py
@@ -4,7 +4,6 @@ import re
 from geo import *
 import math
 
-# METERS_PER_SAMPLE = 30.0
 SAMPLES = 350
 
 def find_intermediate_point(p1, p2, d):
@@ -16,11 +15,8 @@ class ElevationPath(object):
     # SUFFIX = ''
     def __init__(self, path):
         self.path = path
-        # self.samples = round(len(path) / METERS_PER_SAMPLE)
         self.points = self.sample_points()
-
         self.make_elevation_arrays()
-
 
     def sample_points(self):
         seg_size = max(30, len(self.path) / SAMPLES)
@@ -29,32 +25,18 @@ class ElevationPath(object):
         current_sample_length = 0
         points = []
         for i, point in enumerate(self.path):
-            # print "The raw distance remaining in this path is %s" % len(self.path.slice(i))
             if i == 0:
                 current = point
                 continue
-            # what is the distance from current to next
             d_to_next_point = current.distance(point)
-            # print "The distance from the %sth point to the %sth point is %s" % (i-1, i, d_to_next_point)
-            # print "We currently have %s points" % len(points)
-            # print "We have already made a segment that is %s meters long" % current_sample_length
-            # if, having gone to next, our total length is great than segsize
             if current_sample_length + d_to_next_point > seg_size:
                 delta = seg_size - current_sample_length
-                # print "Therefore, we will take a sample %s meters into this new segment" % delta
                 remainder = d_to_next_point - delta
                 # find an intermediate point, which will make the total length segsize
                 p = find_intermediate_point(current, point, delta)
-                c_d = current.distance(p)
-                # print "To check, this point is %s meters from the start of the segment, for a total distance of %s" % (c_d, current_sample_length + c_d)
-
-                # if len(points) > 0:
-                #     check[str(p)] = points[-1].distance(p)
                 points.append(p)
                 while remainder > seg_size:
-                    # print "There are %s meters left in this segment (measured at %s), so we should take %s more points from it" % (remainder, p.distance(point), math.floor(remainder / METERS_PER_SAMPLE))
                     new_p = find_intermediate_point(p, point, seg_size)
-                    # print "To check, this point is %s meters from the last point" % p.distance(new_p)
                     points.append(new_p)
                     remainder -= seg_size
                     p = new_p
@@ -82,7 +64,6 @@ class ElevationPath(object):
         for point in self.points:
             p = (int(math.floor(point.lat)), int(math.floor(point.lon)))
             el = self.arrays[p].elevation_val(point)
-            # does not add elevation if: el == 0 AND already a list AND delta > 100
             if el != 0 or len(elevations) == 0 or elevations[-1] - el < 100:
                 elevations.append(el)
         return elevations
@@ -135,23 +116,3 @@ class ElevationArray(object):
         elif len(els) == 0: return 0
         p0 = np.array([x_d, y_d])
         return self.elevate(p1, p2, p3, p0)
-
-
-
-
-# if __name__ == '__main__':
-#     app = Flask(__name__)
-#     app.config.from_object('settings')
-#     path = sys.argv[1]
-#     name = path.split('/')[-1]
-#     conn = S3Connection(app.config.get('AWS_ACCESS_KEY'), app.config.get('AWS_SECRET_KEY'))
-#     bucket = conn.create_bucket(app.config.get('ELEVATIONS_BUCKET_NAME'))
-#     k = Key(bucket)
-#     k.key = name
-#     k.set_contents_from_filename(path)
-#
-    # path = sys.argv[1]
-    # point = sys.argv[2].split(',')
-    # point = Point(float(point[0]), float(point[1]))
-    # e = ElevationArray(path)
-    # print e.elevation_val(point)
