4281cc84d4258ba1e6516cab57c2d299ad5473bc
Peter France <pfrance@gmail.com>
2013-09-17 15:48:10 -0400

    changed Elevation Array to read bits directly, rather than reading into numpy array

--- elevations.py
+++ elevations.py
@@ -3,6 +3,7 @@ import numpy as np
 import re
 from geo import *
 import math
+import struct
 
 SAMPLES = 350
 
@@ -74,12 +75,9 @@ class ElevationArray(object):
         lat = float(c[0][1:]) * (-1 if c[0][0] == 'S' else 1)
         lng = float(c[1][1:]) * (-1 if c[1][0] == 'W' else 1)
         self.bounds = Box(north = lat + 1, east = lng + 1, south = lat, west = lng)
-        path = os.path.join(self.DIR, filename)
-        siz = os.path.getsize(path)
+        self.path = os.path.join(self.DIR, filename)
+        siz = os.path.getsize(self.path)
         dim = int(math.sqrt(siz/2))
-        assert dim*dim*2 == siz, 'Invalid file size'
-        self.data = np.fromfile(path, np.dtype('>i2'), dim*dim).reshape((dim, dim))
-        np.place(self.data, self.data < 0, 0)
         self.dimx = self.dimy = dim -1
 
     def index_2_point(self, i, j):
@@ -93,6 +91,13 @@ class ElevationArray(object):
         a = np.linalg.solve(x,y)
         return (-1 - a[0] * x0[0] - a[1] * x0[1]) / a[2]
 
+    def read_value(self, x, y):
+        pos = (3601*2*y + 2*x)
+        with open(self.path) as file_object:
+            file_object.seek(pos)
+            (value,) = struct.unpack('>h', file_object.read(2))
+        return value
+
     def elevation_val(self, point):
         x_d = (point.lon - self.bounds.west) * self.dimx
         y_d = self.dimy * (1 - (point.lat - self.bounds.south))
@@ -100,16 +105,65 @@ class ElevationArray(object):
         y_maj = int(round(y_d))
         if x_maj - x_d > 0: x_min = x_maj - 1
         elif x_maj + 1 <= self.dimx: x_min = x_maj + 1
-        else: return self.data[y_maj, x_maj]
+        else: return self.read_value(x_maj, y_maj)
         if y_maj - y_d > 0: y_min = y_maj - 1
         elif y_maj + 1 <= self.dimy: y_min = y_maj + 1
-        else: return self.data[y_maj, x_maj]
-        p1 = np.array([x_maj, y_maj, self.data[y_maj, x_maj]])
-        p2 = np.array([x_maj, y_min, self.data[y_min, x_maj]])
-        p3 = np.array([x_min, y_maj, self.data[y_maj, x_min]])
+        else: return self.read_value(x_maj, y_maj)
+        # self.data[y_maj, x_maj]
+        p1 = np.array([x_maj, y_maj, self.read_value(x_maj, y_maj)])
+        p2 = np.array([x_maj, y_min, self.read_value(x_maj, y_min)])
+        p3 = np.array([x_min, y_maj, self.read_value(x_min, y_maj)])
         els = [l for l in [p1[2], p2[2], p3[2]] if l != 0]
         if len(els) == 2: return sum(els) / 2
         elif len(els) == 1: return sum(els)
         elif len(els) == 0: return 0
         p0 = np.array([x_d, y_d])
         return self.elevate(p1, p2, p3, p0)
+
+# class ElevationArrayOld(object):
+#     DIR = 'static/elevations/'
+#
+#     def __init__(self, filename):
+#         c = re.findall(r'[A-Z][0-9]+', filename)
+#         lat = float(c[0][1:]) * (-1 if c[0][0] == 'S' else 1)
+#         lng = float(c[1][1:]) * (-1 if c[1][0] == 'W' else 1)
+#         self.bounds = Box(north = lat + 1, east = lng + 1, south = lat, west = lng)
+#         path = os.path.join(self.DIR, filename)
+#         siz = os.path.getsize(path)
+#         dim = int(math.sqrt(siz/2))
+#         assert dim*dim*2 == siz, 'Invalid file size'
+#         self.data = np.fromfile(path, np.dtype('>i2'), dim*dim).reshape((dim, dim))
+#         np.place(self.data, self.data < 0, 0)
+#         self.dimx = self.dimy = dim -1
+#
+#     def index_2_point(self, i, j):
+#         lng = self.bounds.west + (float(j) / self.dimx)
+#         lat = self.bounds.south + (float(self.dimy - i) / self.dimy)
+#         return Point(lat, lng)
+#
+#     def elevate(self, x1, x2, x3, x0):
+#         x = np.array([x1,x2,x3])
+#         y = np.array([-1,-1,-1])
+#         a = np.linalg.solve(x,y)
+#         return (-1 - a[0] * x0[0] - a[1] * x0[1]) / a[2]
+#
+#     def elevation_val(self, point):
+#         x_d = (point.lon - self.bounds.west) * self.dimx
+#         y_d = self.dimy * (1 - (point.lat - self.bounds.south))
+#         x_maj = int(round(x_d))
+#         y_maj = int(round(y_d))
+#         if x_maj - x_d > 0: x_min = x_maj - 1
+#         elif x_maj + 1 <= self.dimx: x_min = x_maj + 1
+#         else: return self.data[y_maj, x_maj]
+#         if y_maj - y_d > 0: y_min = y_maj - 1
+#         elif y_maj + 1 <= self.dimy: y_min = y_maj + 1
+#         else: return self.data[y_maj, x_maj]
+#         p1 = np.array([x_maj, y_maj, self.data[y_maj, x_maj]])
+#         p2 = np.array([x_maj, y_min, self.data[y_min, x_maj]])
+#         p3 = np.array([x_min, y_maj, self.data[y_maj, x_min]])
+#         els = [l for l in [p1[2], p2[2], p3[2]] if l != 0]
+#         if len(els) == 2: return sum(els) / 2
+#         elif len(els) == 1: return sum(els)
+#         elif len(els) == 0: return 0
+#         p0 = np.array([x_d, y_d])
+#         return self.elevate(p1, p2, p3, p0)
